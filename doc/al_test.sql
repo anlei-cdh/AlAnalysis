80f0ff6a731d24b55470721c6aec9ca1baff87ad
anlei <anlei@ihangmei.com>
2017-11-13 16:26:06 +0800

    add config

--- doc/al_test.sql
+++ doc/al_test.sql
@@ -34,6 +34,7 @@ SELECT * FROM `mllib_channel_data`;
 
 ================================================================================
 
+#OLAP
 hdfs dfs -rm -r /logs/aura
 hdfs dfs -rm -r /logs/aura_parquet
 drop table aura;
@@ -42,11 +43,14 @@ invalidate metadata;
 
 ================================================================================
 
+#Hive MapReduce
 UPDATE `hive_dimension_data` SET pv = 0,uv = 0,ip = 0 WHERE DAY = '2016-12-07';
+UPDATE `hive_dimension_data` SET time = 0 WHERE DAY = '2016-12-07';
 SELECT * FROM `hive_dimension_data` WHERE `day` = '2016-12-07';
 
 ================================================================================
 
+#Storm
 TRUNCATE `storm_dimension_data`;
 TRUNCATE `storm_content_data`;
 TRUNCATE `storm_content_detail`;
@@ -55,6 +59,7 @@ flume-ng avro-client -H cdh01 -p 9999 -F /logs/aura.log
 
 ================================================================================
 
+#Kudu
 select url,title from aura_parquet limit 5;
 update aura_parquet set url = 'http://www.baidu.com/';
 update aura_kudu set count = 40,rate = 10 where id = 2;
@@ -62,4 +67,10 @@ update aura_kudu set count = 29,rate = 3 where id = 2;
 
 ================================================================================
 
-UPDATE `hive_dimension_data` SET time = 0 WHERE DAY = '2016-12-07';
+#Kafka
+-kafka创建topic
+/opt/cloudera/parcels/KAFKA/bin/kafka-topics --create --zookeeper cdh01:2181,cdh02:2181,cdh03:2181 --replication-factor 1 --partitions 1 --topic aura
+-kafka查看topic
+/opt/cloudera/parcels/KAFKA/bin/kafka-topics --list --zookeeper cdh01:2181,cdh02:2181,cdh03:2181
+-kafka创建consumer
+/opt/cloudera/parcels/KAFKA/bin/kafka-console-consumer --zookeeper cdh01:2181,cdh02:2181,cdh03:2181 --topic aura --from-beginning
