33c8ffe964dd41352b73c505910bce7ba747b86e
anlei <anlei@ihangmei.com>
2017-12-01 17:07:27 +0800

    ml lr dt

--- AlSpark/src/main/scala/com/al/spark/ml/DecisionTree.scala
+++ AlSpark/src/main/scala/com/al/spark/ml/DecisionTree.scala
@@ -1,6 +1,12 @@
 package com.al.spark.ml
 
+import java.sql.{Connection, PreparedStatement}
+
+import com.al.basic.BasicDao
 import com.al.config.Config
+import com.al.db.DBHelper
+import com.al.entity.DataResult
+import com.al.spark.ml.LogisticRegression.Lr
 import com.al.util.{MLUtil, TrainingUtil, WordSplitUtil}
 import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, DecisionTreeClassifier, LogisticRegression, LogisticRegressionModel}
 import org.apache.spark.sql.SparkSession
@@ -51,10 +57,59 @@ object DecisionTree {
 //
 //    result.show(false)
 
-    saveDecisionTreeModel(spark)
-    testDecisionTree(spark)
+    // saveDecisionTreeModel(spark)
+    // testDecisionTree(spark)
+
+    processDecisionTree(spark)
 
     spark.stop()
+  }
+
+  def processDecisionTree(spark: SparkSession): Unit = {
+    val dataframe = spark.read.json(Config.input_path)
+    val selectdf = dataframe.selectExpr("uuid", "ip", "title", "title AS text")
+
+    import spark.implicits._
+    val dataset = selectdf.as[Lr]
+    val wordsplit = dataset.map{lr =>
+      lr.text = WordSplitUtil.getWordSplit(lr.title)
+      lr
+    }.filter(lr => lr.text != null).select("uuid","ip",Config.text)
+
+    val idf = MLUtil.hashingFeatures(wordsplit, Config.numFeatures).select("uuid","ip",Config.features)
+
+    val model = DecisionTreeClassificationModel.load(Config.dt_path)
+    val prediction = model.transform(idf).select("uuid","ip","prediction")
+
+    prediction.createOrReplaceTempView("dftable")
+    val result = spark.sql("SELECT prediction,COUNT(1) pv,COUNT(DISTINCT(uuid)) uv,COUNT(DISTINCT(ip)) ip FROM dftable GROUP BY prediction")
+
+    result.show(false)
+
+//    result.foreachPartition(records => {
+//      if (!records.isEmpty) {
+//        val conn: Connection = DBHelper.getConnectionAtFalse()
+//        val sql: String = "INSERT INTO mllib_gender_data(genderid,`day`,pv,uv,ip) VALUES (#{prediction},#{day},#{pv},#{uv},#{ip}) on duplicate key update pv = values(pv),uv = values(uv),ip = values(ip)"
+//        val pstmt: PreparedStatement = conn.prepareStatement(BasicDao.getRealSql(sql))
+//        var count: Int = 0
+//
+//        records.foreach {
+//          record => {
+//            val dataResult = new DataResult
+//            dataResult.prediction = record.getAs[Double]("prediction").toInt
+//            dataResult.pv = record.getAs[Long]("pv").toInt
+//            dataResult.uv = record.getAs[Long]("uv").toInt
+//            dataResult.ip = record.getAs[Long]("ip").toInt
+//            dataResult.day = Config.day
+//
+//            count += 1
+//            DBHelper.setPreparedSqlexecuteBatch(conn, pstmt, sql, count, dataResult)
+//          }
+//        }
+//
+//        DBHelper.commitClose(conn, pstmt)
+//      }
+//    })
   }
 
   def saveDecisionTreeModel(spark: SparkSession): Unit = {
