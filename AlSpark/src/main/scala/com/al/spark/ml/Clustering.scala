fac7717a479ca0f4d453f577aa2420a00eecd999
anlei <anlei@ihangmei.com>
2017-12-06 14:42:00 +0800

    ml clustering

--- AlSpark/src/main/scala/com/al/spark/ml/Clustering.scala
+++ AlSpark/src/main/scala/com/al/spark/ml/Clustering.scala
@@ -1,19 +1,35 @@
 package com.al.spark.ml
 
-import com.al.util.{MLUtil, TrainingUtil}
+import com.al.util.MLUtil
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.sql.SparkSession
 
 object Clustering {
 
+  case class Users(label: String, textlist: List[String], var text: String)
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().master("local").appName(s"${this.getClass.getSimpleName}").getOrCreate()
 
-    val numFeatures = 20
+    val numFeatures = 50
+    val k = 5
     /**
       * 训练集
       */
-    val clusteringDataFrame = spark.createDataFrame(TrainingUtil.clusteringData).toDF("label", "text")
+    DecisionTree.saveDecisionTreeModel(spark)
+    val predictionDataFrame = DecisionTree.processDecisionTree(spark).filter("uuid is not null")
+
+    import org.apache.spark.sql.functions._
+    val df = predictionDataFrame.groupBy("uuid").agg(collect_list("prediction") as "prediction").selectExpr("uuid AS label", "uuid AS text", "prediction AS textlist")
+
+    import  spark.implicits._
+    val ds = df.as[Users]
+
+    val clusteringDataFrame = ds.map(user => {
+      user.text = user.textlist.mkString(" ")
+      user
+    }).toDF()
+
     /**
       * 分词,向量化
       */
@@ -22,7 +38,7 @@ object Clustering {
     /**
       * K-means模型
       */
-    val kmeans = new KMeans().setK(3).setSeed(1L)
+    val kmeans = new KMeans().setK(k).setSeed(1L)
     val model = kmeans.fit(clustering)
 
     /**
@@ -32,7 +48,7 @@ object Clustering {
     /**
       * 聚类结果
       */
-    model.transform(clustering).show(false)
+    model.transform(clustering).show(100)
 
     spark.stop()
   }
