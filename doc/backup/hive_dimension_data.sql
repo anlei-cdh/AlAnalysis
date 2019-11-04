529cffed668eb19e93cc93398aa56ef1e48923b6
anlei-cdh <824652977@qq.com>
2019-11-04 22:17:22 +0800

    backup data

---
+++ doc/backup/hive_dimension_data.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Ultimate v11.13 (64 bit)
+MySQL - 5.1.73 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `hive_dimension_data` (`day`, `pv`, `uv`, `ip`, `time`) values('2016-12-01','759','267','339','2673');
+insert into `hive_dimension_data` (`day`, `pv`, `uv`, `ip`, `time`) values('2016-12-02','1120','400','599','4203');
+insert into `hive_dimension_data` (`day`, `pv`, `uv`, `ip`, `time`) values('2016-12-03','1542','950','533','9502');
+insert into `hive_dimension_data` (`day`, `pv`, `uv`, `ip`, `time`) values('2016-12-04','1331','467','826','5010');
+insert into `hive_dimension_data` (`day`, `pv`, `uv`, `ip`, `time`) values('2016-12-05','1062','367','549','3737');
+insert into `hive_dimension_data` (`day`, `pv`, `uv`, `ip`, `time`) values('2016-12-06','751','588','422','5880');
+insert into `hive_dimension_data` (`day`, `pv`, `uv`, `ip`, `time`) values('2016-12-07','1058','188','659','1884');
